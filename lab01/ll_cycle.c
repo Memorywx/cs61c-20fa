34a8b81a8c5e61afe12fd7a919c4d06e54aa1b79
Heliannl <gyy7314@outlook.com>
2022-04-14 20:54:58 +0800

    Exercise 5 done

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,14 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node *tortoies = head, *hare = head;
+    do {
+        if (hare == NULL || hare->next == NULL || hare->next->next == NULL)
+            return 0;
+        hare = hare->next->next;
+        tortoies = tortoies->next;
+        if (hare == tortoies) 
+            return 1;
+    } while (hare != tortoies);
     return 0;
 }
