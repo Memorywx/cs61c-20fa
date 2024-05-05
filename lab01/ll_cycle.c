c9744ec14ec219f6708630022c159c339e87c61e
Memorywx <2829327570@qq.com>
2024-05-05 14:58:41 +0800

    lab01 done

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,13 +2,23 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    node *tortoies, *hare;
-    tortoies = hare = head;
-    while (hare && hare->next) {
-        hare = hare->next->next;
-        tortoies = tortoies->next;
-        if (hare == tortoies) 
+    // your code here
+    if (head == NULL) {
+        return 0;
+    }
+    node *tortoise = head;
+    node *hare = head;
+    while (hare != NULL) {
+        hare = hare->next;
+        if (hare != NULL) {
+            hare = hare->next;
+        } else {
+            return 0;
+        }
+        tortoise = tortoise->next;
+        if (tortoise == hare) {
             return 1;
+        }
     }
     return 0;
 }
